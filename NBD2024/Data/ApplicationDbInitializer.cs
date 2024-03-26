7693873ab6908e05f12469ea6d9dc0833eabe1be
Jordan Lucciola <jlucciola1@ncstudents.niagaracollege.ca>
2024-03-26 16:31:14 -0400

    Added user authentication/log in (Not done)

---
+++ NBD2024/Data/ApplicationDbInitializer.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace NBD2024.Data
+{
+    public static class ApplicationDbInitializer
+    {
+        public static async void Seed(IApplicationBuilder applicationBuilder)
+        {
+            ApplicationDbContext context = applicationBuilder.ApplicationServices.CreateScope()
+                .ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            try
+            {
+                //Create the database if it does not exist and apply the migration
+                context.Database.Migrate();
+
+                //Create Roles
+                var RoleManager = applicationBuilder.ApplicationServices.CreateScope()
+                    .ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                string[] roleNames = { "Admin", "Sales", "Designer", "Management" };
+                IdentityResult roleResult;
+                foreach ( var roleName in roleNames )
+                {
+                    var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                    if (!roleExist)
+                    {
+                        roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                    }
+                }
+
+                //Create Users
+                var userManager = applicationBuilder.ApplicationServices.CreateScope()
+                    .ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                if (userManager.FindByEmailAsync("admin@outlook.com").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "amin@outlook.com",
+                        Email = "amin@outlook.com"
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "P@55w@rd").Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Admin").Wait();
+                    }
+                }
+
+                if (userManager.FindByEmailAsync("sales@outlook.com").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "sales@outlook.com",
+                        Email = "sales@outlook.com"
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "P@55w@rd").Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Sales").Wait();
+                    }
+                }
+
+                if (userManager.FindByEmailAsync("designer@outlook.com").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "designer@outlook.com",
+                        Email = "designer@outlook.com"
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "P@55w@rd").Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Designer").Wait();
+                    }
+                }
+
+                if (userManager.FindByEmailAsync("management@outlook.com").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "management@outlook.com",
+                        Email = "management@outlook.com"
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "P@55w@rd").Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Management").Wait();
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.GetBaseException().Message);
+            }
+
+        }
+    }
+}
--- NBD2024/Program.cs
+++ NBD2024/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NBD2024.Data;
-
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,11 +16,52 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDbContext<NBDContext>(options =>
     options.UseSqlite(connectionString));
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    // Cookie settings
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.SlidingExpiration = true;
+});
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
 
 var app = builder.Build();
 
